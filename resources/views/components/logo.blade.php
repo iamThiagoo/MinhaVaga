a1a96ea9f862f6785124166c1052d0036eb56333
Thiago Ferreira <devthiagoferreira@gmail.com>
2023-02-23 11:01:54 -0300

    Breeze implementado e início da construção das views

---
+++ resources/views/components/logo.blade.php
@@ -0,0 +1,11 @@
+<div>
+    <a href="{{ route('home') }}" class="flex items-center gap-1 hover:opacity-90 delay-75">
+        <img 
+            class="w-24"
+            src="{{ asset('images/logo.png') }}" 
+        />
+        <h1 class="text-xl -mt-2 text-sky-800 font-black">
+            MinhaVaga.com
+        </h1>
+    </a>
+</div>
---
+++ resources/views/components/navigation.blade.php
@@ -0,0 +1,38 @@
+<nav>
+    <div class="hidden md:flex flex-1 mx-auto pt-1 justify-between container border-2 border-rose-500">
+        <x-logo />
+    
+        <div class="flex -mt-1">
+            <ul class="flex flex-1 gap-12 items-center">
+                <li>
+                    <a href="#" class="text-lg text-gray-900 font-black hover:opacity-90">Início</a>
+                </li>
+                <li>
+                    <a href="#" class="text-lg text-gray-900 font-black hover:opacity-90">Vagas</a>
+                </li>
+                <li>
+                    <a href="#" class="text-lg text-gray-900 font-black hover:opacity-90">Anunciar</a>
+                </li>
+            </ul>
+        </div>
+    
+        <div class="flex -mt-1">
+            <ul class="flex flex-1 gap-7 items-center">
+                <li>
+                    <a href="#" class="text-lg text-gray-900 font-black hover:opacity-90">Login</a>
+                </li>
+                <li>
+                    <a href="#" class="text-lg bg-gray-900 text-white p-2 rounded-md font-bold hover:opacity-90">Cadastre-se</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="p-2">
+        <x-logo />
+
+        <div >
+
+        </div>
+    </div>
+</nav>
