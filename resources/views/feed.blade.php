c8964a53a729e3c530ada0ba2307f353b157653d
Thiago Ferreira <devthiagoferreira@gmail.com>
2023-05-01 17:00:58 -0300

    Adjust in search

--- resources/views/feed.blade.php
+++ resources/views/feed.blade.php
@@ -18,42 +18,7 @@
                         </svg>
                         <input id="search-feed" class="w-full p-0 border-0" type="text" name="search" placeholder="Pesquise por vagas, pessoas, publicações..." required autofocus />
                     </label>
-                    <div class="">
-                        <ul class='px-3 py-1 rounded-b bg-gray-100 border-x border-b border-gray-400 -mt-2'>
-                            <li class="px-3 pt-3 pb-2 border-b border-gray-400">
-                                <a href="" class="flex items-center w-full gap-3 hover:opacity-80">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-                                    </svg> 
-                                    fsdfsdf
-                                </a>
-                            </li>
-                            <li class="px-3 pt-3 pb-2 border-b border-gray-400">
-                                <a href="" class="flex items-center w-full gap-3 hover:opacity-80">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-                                    </svg> 
-                                    fsdfsdf
-                                </a>
-                            </li>
-                            <li class="px-3 pt-3 pb-2 border-b border-gray-400">
-                                <a href="" class="flex items-center w-full gap-3 hover:opacity-80">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-                                    </svg> 
-                                    fsdfsdf
-                                </a>
-                            </li>
-                            <li class="px-3 pt-3 pb-2 border-gray-400">
-                                <a href="" class="flex items-center w-full gap-3 hover:opacity-80">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-                                    </svg> 
-                                    fsdfsdf
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
+                    <div class="search-list hidden"></div>
                 </div>
             </div>
 
@@ -107,7 +72,7 @@
                     </button>
                 </div>
 
-                <div class="grid gap-5 lg:grid-cols-2 lg:gap-5 xl:gap-2 2xl:grid-cols-3 2xl:gap-3"">
+                <div class="grid gap-5 lg:grid-cols-2 lg:gap-5 xl:gap-2 2xl:grid-cols-3 2xl:gap-3">
                     <x-opportunity-card />
                     <x-opportunity-card />
                     <x-opportunity-card />
@@ -152,10 +117,11 @@
         document.querySelector('#search-feed').addEventListener('keyup', (event) => {
             if (event.target.value.length >= 2) {
                 requestGlobalSearch(event.target.value);
-
                 document.querySelector("#label-search-feed").classList.remove('rounded');
+                document.querySelector('.search-list').classList.remove('hidden');
             } else {
                 document.querySelector("#label-search-feed").classList.add('rounded');
+                document.querySelector('.search-list').classList.add('hidden');
             }
         });
 
@@ -169,22 +135,46 @@
                 },
                 dataType: 'json',
                 success: function(results){
-                    var result = "<ul class='fixed bg-red-500'>";
+                    var result = document.createElement('ul');
+                    result.classList.add('px-3', 'py-1', 'rounded-b', 'bg-gray-100', 'border-x', 'border-b', 'border-gray-400', '-mt-2');
+                    var count = 0;
 
-                    $(results).each( function(index, key) {
-                        $(key).each(function (index, value) {
-                            result += "<li>" + value.name + "</li>";
-                            // if (key == 0 || key == 3) {
+                    for(i = 0; i < results.length; i++) {
+                        if (results[i].length > 0) {
+                            results[i].forEach( function (item) {
+                                if (count <  6){
+                                    result.appendChild( createItemSearch(item) );
+                                    ++count;
+                                }
+                            });
+                        }
+                    }
 
-                            // }
-                        });
-                    });
+                    if (result.childNodes.length === 0) {
+                        result.innerHTML = `<p class='text-center py-3'> Nenhum resultado encontrado! </p>`;
+                    }
 
-                    result += "</ul>";
-
-                    document.querySelector('#search-feed').innerHTML = result;
+                    document.querySelector('.search-list').innerHTML = "";
+                    document.querySelector('.search-list').appendChild(result);
                 }
             });
+        }
+
+        function createItemSearch (value)
+        {
+            var item = document.createElement('li');
+            item.classList.add("px-3", "pt-3", "pb-2");
+
+            var link = document.createElement('a');
+            link.classList.add("flex", "items-center", "w-full",  "gap-3", "hover:opacity-80", 'cursor-pointer');
+
+            link.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+            </svg> ${value.name}`;
+
+            item.appendChild(link);
+            return item;
         }
 
     </script>
